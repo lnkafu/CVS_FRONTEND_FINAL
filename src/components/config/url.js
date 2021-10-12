aa17e6a0f45cd7fa6214fb23da8c7a7ca8545af5
unknown <lawnkafu@gmail.com>
2021-10-11 22:27:23 -0400

    minor updates

--- src/components/config/url.js
+++ src/components/config/url.js
@@ -1,4 +1,4 @@
 
 //exports.url = "http://localhost:3012" // local server
 
-exports.url = "https://c0cc-71-163-44-204.ngrok.io" // deployment server for backend code
+exports.url = "https://9957-2601-145-8180-4820-00-85dc.ngrok.io" // deployment server for backend code
