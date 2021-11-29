c3dcd767e41ff1fc8c692d0c15bd957b65898166
unknown <lawnkafu@gmail.com>
2021-11-29 13:06:12 -0500

    minor updates

--- src/components/viewInventory/ViewInventoryEnRoute.componentCustomerVersion.jsx
+++ src/components/viewInventory/ViewInventoryEnRoute.componentCustomerVersion.jsx
@@ -93,7 +93,7 @@ export default class ViewInventoryEnRouteCustomerVersionComponent extends React.
                     <div align='center' ><h1>CVS Store</h1></div>
                     <div align='center' ><h6>Située au COLLEGE L'AGAPE CITE CICAM, </h6></div>
                     <div align='center' ><h6>Numero de Magasin/ Store Number: 651-692-518</h6></div>
-                    <div align='center' ><h6>Whatsapp: 79543644\679-700-008</h6></div>
+                    <div align='center' ><h6>Whatsapp: 79543644/679-700-008</h6></div>
                         <h6> <button className='btn btn-dark btn-rounded' onClick={this.reGetList}> Refresh List </button> Inventaire/Inventory En Route </h6>
                         <div className="input-group mb-3">
                             <input type="text" name='searchField' onChange={this.handleChange} className="form-control" placeholder="Search Inventory Record By ID, Item type OR Item Model" aria-describedby="basic-addon2" />
