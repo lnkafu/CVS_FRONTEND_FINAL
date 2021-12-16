b1a6ab256c16df4a58eb6d9607bd4e94f2fb7408
unknown <lawnkafu@gmail.com>
2021-12-16 10:05:49 -0500

    ngrok url changed

--- src/components/config/url.js
+++ src/components/config/url.js
@@ -1,4 +1,4 @@
 
 //exports.url = "http://localhost:3012" // local server
 
-exports.url = "https://5bdf-2601-145-8180-4820-00-353.ngrok.io" // deployment server for backend code
+exports.url = "https://d3e0-2601-145-8180-4820-00-353.ngrok.io" // deployment server for backend code
