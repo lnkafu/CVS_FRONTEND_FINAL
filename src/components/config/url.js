c4edae50d63147395587114050070e2699880123
unknown <lawnkafu@gmail.com>
2022-07-25 07:13:07 -0400

    url changed

--- src/components/config/url.js
+++ src/components/config/url.js
@@ -1,4 +1,4 @@
 
 //exports.url = "http://localhost:3012" // local server
 
-exports.url = "https://5227-2601-43-8201-4e30-7c0e-5048-1945-9f7c.ngrok.io" // deployment server for backend code
+exports.url = "https://cfc7-2601-43-8201-4e30-b57f-6726-676b-9f94.ngrok.io" // deployment server for backend code
